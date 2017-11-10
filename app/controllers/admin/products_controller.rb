e905773a50086c9c9abd944daab16e99cde50653
Dilan <dilannebioglu@gmail.com>
2017-11-10 00:07:33 +0000

    http authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['name'], password: ENV['password']
+
 
   def index
     @products = Product.order(id: :desc).all
