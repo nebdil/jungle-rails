c166d40b0d024cd334a91563528c155c13371512
Dilan <dilannebioglu@gmail.com>
2017-11-13 22:57:17 +0000

    merge conflict residues

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,7 @@
 class UserMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
-<<<<<<< HEAD
-  def welcome_email(user)
-    @user = User.first
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-=======
   def welcome_email(order)
     @order = order
     mail(to: @order.email, subject: "Your order ID is: #{@order.id}")
->>>>>>> feature/order-receipt
   end
 end
